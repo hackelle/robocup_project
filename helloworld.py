4f3b4112e0fe95d1c6ef23fa35e5270259c8bd26
Vito Mengers <v.mengers@campus.tu-berlin.de>
2018-08-31 13:45:53 +0200

    helloworld stuff

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,18 @@
 from naoqi import ALProxy
+import numpy as np
+import cv2
+import matplotlib.pyplot as plot
+
+
+
 # tts = ALProxy("ALTextToSpeech", "nao2.local", 9559)
-tts = ALProxy("ALTextToSpeech", "10.0.7.12", 9559)
-pic = ALProxy("RobocupVision", "10.0.7.13", 9559)
-pic.takePicture("./", "test.png")
-tts.say("Click")
-#tts.say("Good day, sire!")
+# tts = ALProxy("ALTextToSpeech", "10.0.7.16", 9559)
+# tts.say("Click")
+vision = ALProxy('RobocupVision', '10.0.7.15', 9559)
+
+cameraId = 0 # 0 for topcam
+data = vision.getBGR24Image(cameraId)
+image = np.fromstring(data, dtype=np.uint8).reshape((480, 640, 3))
+rgb_img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+plot.imshow(rgb_img)
+print "done"
