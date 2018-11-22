d22c5595966771d5303a6273c5038a273d55f878
Jasper v. B <jasper@mezzo.de>
2018-11-22 13:15:02 +0100

    Calculate & draw FOV & position

--- gui/vision.py
+++ gui/vision.py
@@ -6,6 +6,7 @@ from math import floor, ceil, sqrt, sin, cos, pi
 import logging
 import os
 from threading import Lock, Condition
+from collections import namedtuple
 
 from PyQt5 import QtGui, QtCore
 import tensorflow as tf
@@ -27,6 +28,9 @@ INTERSECTION_MIN = 0.2
 # Factor we increase the bounding boxes by in each direction
 BB_MULT = 1.25
 FRAMERATE = 1/5.0
+
+
+RobotFace = namedtuple("RobotFace", "eyes ear box")
 
 
 class ImageProvider(object):
@@ -272,11 +276,11 @@ class Vision(QtCore.QObject):
             self.logger.debug("Detecting ellipses in Box #%i...", i)
             ellipse_detection = EllipseDetection(processed[-1], edges[-1])
             face = ellipse_detection.detect_ellipses()
-            faces.append(face)
-            ellipse_detection.draw_ellipses(*face[0])
+            ellipse_detection.draw_ellipses(*face)
+            faces.append(RobotFace(eyes=face[0], ear=face[1], box=box['box']))
             self.logger.debug("Done!")
         self.logger.debug("Creating Geometry...")
-        geometry_creation = GeometryCreation(faces)
+        geometry_creation = GeometryCreation(faces, img.shape[:2])
         geometry = geometry_creation.create()
         geometry_img = geometry_creation.draw(geometry)
 
@@ -309,7 +313,7 @@ class Vision(QtCore.QObject):
             # temp = self.postprocess(img)
             # edges = self.edge_detection(temp)
             temp, edges, scores, geometry_img = self.detect_heads(img)
-            geometry_img = self.make_pixmap(img)
+            geometry_img = self.make_pixmap(geometry_img)
             img = self.make_pixmap(img)
             edges = map(self.make_pixmap, edges)
             temp = map(self.make_pixmap, temp)
@@ -362,7 +366,7 @@ class EllipseDetection(object):
         self.edge_points = np.array(self.edge_points)
         ellipses = self.filter_good_ellipses(contours)
         ellipses = self.filter_overlapping(ellipses)
-        return self.facial_structure(ellipses), (self.rows, self.cols)
+        return self.facial_structure(ellipses)
 
     def draw_ellipses(self, eyes, ear):
         for eye in eyes:
@@ -616,50 +620,74 @@ class EllipseDetection(object):
 NAO_EAR_HEIGHT = 62.1  # mm
 NAO_IMG_HEIGHT = 480  # px
 NAO_SENSOR_HEIGHT = 968 * 1.9E-3  # mm
-NAO_VFOV = 47.64 / 180 * pi
+NAO_HFOV = 60.97 / 180 * pi
+NAO_VFOV = 47.64 / 180 * np.pi
 NAO_FOCAL_LENGTH = ((NAO_SENSOR_HEIGHT / 2) / sin(NAO_VFOV/2)
                     * sin(pi/2 - NAO_VFOV/2))
 
 
 class GeometryCreation(object):
-    def __init__(self, faces):
+    def __init__(self, faces, img_shape):
         self.faces = faces
+        self.img_shape = img_shape
         self.robots = []
         self.logger = logging.getLogger()
 
     def create(self):
+        geometry = []
         for i, face in enumerate(self.faces):
-            eyes, ear = face[0]
-            if ear is None:
+            if face.ear is None:
                 self.logger.warn("Face %i has no ear, aborting!", i)
                 continue
 
-            height = ear[1][1]
+            height = face.ear[1][1]
             dist = (
                 (NAO_FOCAL_LENGTH * NAO_EAR_HEIGHT * NAO_IMG_HEIGHT) /
                 (height * NAO_SENSOR_HEIGHT)
             )
+            radial_angle = self.calculate_radial_angle(face.box)
+            self.logger.debug("Radial: {}, {}".format(dist, radial_angle))
+            location = (
+                dist * np.cos(radial_angle),
+                dist * np.sin(radial_angle)
+            )
+            self.logger.debug("Cartesian: {}, {}".format(location[0],
+                                                         location[1]))
 
             # Calculate angle (between [0, 180] deg)
-            angle, sure = self.calculate_angle(face)
+            facing_angle, sure = self.calculate_facing_angle(face)
+            self.logger.debug("local facing angle = {}".format(facing_angle))
+            facing_angle = radial_angle - facing_angle
+            # facing_angle = np.pi / 2 - facing_angle
 
-            self.logger.debug("d = {}, angle = {}, sure = {}".format(
-                dist, angle, sure))
+            geometry.append((location, facing_angle))
 
-    def calculate_angle(self, face):
-        (eyes, ear), (rows, cols) = face
+            self.logger.debug("facing angle = {}, sure = {}".format(
+                facing_angle, sure))
 
+        return geometry
+
+    def calculate_radial_angle(self, box):
+        rows = box[2] - box[0]
+        x_center = (box[0] + rows / 2.0) / self.img_shape[0] - 0.5
+        fov_angle = -x_center * NAO_HFOV / 2
+        self.logger.debug("x_center={}\tfov_angle={}\tFOV={}".format(
+            x_center, fov_angle, NAO_HFOV))
+        return fov_angle + np.pi / 2
+
+    def calculate_facing_angle(self, face):
         # This will return the angle from the (local!) x axis...
-        angle = np.arccos(ear[1][0] / ear[1][1])
+        angle = np.arccos(face.ear[1][0] / face.ear[1][1])
         self.logger.debug("angle before correction: {}".format(angle))
         # ... so subtract it from 90 deg
         angle = np.pi / 2 - angle
 
-        if len(eyes) == 0:
+        if len(face.eyes) == 0:
             # The robot is looking away from us, so add 180
             angle += np.pi
 
-        ear_location = self.ear_location(ear, rows)
+        rows = face.box[2] - face.box[0]
+        ear_location = self.ear_location(face.ear, rows)
         if ear_location == 'right':
             angle = -angle
         sure = ear_location != 'unsure'
@@ -677,4 +705,28 @@ class GeometryCreation(object):
             return 'middle'
 
     def draw(self, geometry):
-        pass
+        IMG_WIDTH = 480
+        IMG_HEIGHT = 480
+        img = np.zeros((IMG_WIDTH, IMG_HEIGHT, 3), np.uint8)
+        cv2.circle(img, (IMG_WIDTH / 2, IMG_HEIGHT), 11, (255, 0, 0), -1)
+
+        for robot in geometry:
+            location, facing = robot
+            center = (
+                int(round(IMG_WIDTH / 4000.0 * location[0] + IMG_WIDTH / 2.0)),
+                IMG_HEIGHT - int(round(IMG_WIDTH / 4000.0 * location[1]))
+            )
+            self.logger.debug("center [px]: {}".format(center))
+            cv2.circle(img, center, 5, (0, 255, 0), -1)
+
+            fov_1 = (
+                int(round(center[0] + 600 * np.cos(facing + NAO_HFOV / 2))),
+                int(round(center[1] + 600 * np.sin(facing + NAO_HFOV / 2)))
+            )
+            fov_2 = (
+                int(round(center[0] + 600 * np.cos(facing - NAO_HFOV / 2))),
+                int(round(center[1] + 600 * np.sin(facing - NAO_HFOV / 2)))
+            )
+            cv2.line(img, center, fov_1, (0, 0, 255), 1)
+            cv2.line(img, center, fov_2, (0, 0, 255), 1)
+        return img
